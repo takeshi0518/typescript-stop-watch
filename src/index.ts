10170a061b95573a7afa8ab570c1cfa8c27c408e
takeshi0518 <y.takeshi0518@gmail.com>
2025-10-17 15:59:20 +0900

    feature: タイマーロジック実装完了

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './public/style.css';
 const start = document.getElementById('btn-start') as HTMLElement;
 const stop = document.getElementById('btn-stop') as HTMLElement;
 const reset = document.getElementById('btn-reset') as HTMLElement;
+const display = document.getElementById('display') as HTMLElement;
 
 let elapsed: number = 0;
 let intervalId: number | undefined;
@@ -16,7 +17,9 @@ const startTimer: () => void = () => {
     let pre = new Date().getTime();
     elapsed += pre - now;
     now = pre;
-  }, 1000);
+
+    updateDisplay();
+  }, 10);
 };
 
 const stopTimer: () => void = () => {
@@ -28,6 +31,22 @@ const resetTimer: () => void = () => {
   clearInterval(intervalId);
   intervalId = undefined;
   elapsed = 0;
+
+  updateDisplay();
+};
+
+const updateDisplay: () => void = () => {
+  const ms = Math.floor((elapsed % 1000) / 10);
+  const s = Math.floor(elapsed / 1000) % 60;
+  const m = Math.floor(elapsed / (1000 * 60)) % 60;
+  const h = Math.floor(elapsed / (1000 * 60 * 60));
+
+  const msString = ms.toString().padStart(2, '0');
+  const sString = s.toString().padStart(2, '0');
+  const mString = m.toString().padStart(2, '0');
+  const hString = h.toString().padStart(2, '0');
+
+  display.innerHTML = `${hString}:${mString}:${sString}.${msString}`;
 };
 
 start.addEventListener('click', startTimer);
